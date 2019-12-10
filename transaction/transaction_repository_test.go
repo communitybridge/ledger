06ad96eb73d5d8e47b13a8df8939f76d862d68b9
Paul Ryan <pol.oriain@gmail.com>
2019-12-10 19:49:41 +0900

    fix createTransaction test
    Signed-off-by: Paul Ryan <pol.oriain@gmail.com>

--- transaction/transaction_repository_test.go
+++ transaction/transaction_repository_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/communitybridge/ledger/gen/models"
 	"github.com/communitybridge/ledger/gen/restapi/operations/transactions"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -36,6 +37,20 @@ func TestTransactionGetByID(t *testing.T) {
 	assert.Equal(t, externalTransactionID, transaction.ExternalTransactionID)
 }
 
+func TestTransactionGetTransactionCount(t *testing.T) {
+	conn, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
+	require.Nil(t, err)
+	defer conn.Close()
+	prepareTestDatabase()
+
+	numberOfTransactions := int64(2)
+
+	transactionRepository := NewRepository(conn)
+	transactionCount, err := transactionRepository.GetTransactionCount(context.Background())
+	assert.Nil(t, err)
+	assert.Equal(t, transactionCount, numberOfTransactions)
+}
+
 func TestTransactionListTransactions(t *testing.T) {
 	conn, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
 	require.Nil(t, err)
@@ -55,4 +70,61 @@ func TestTransactionListTransactions(t *testing.T) {
 	transactions, err := transactionRepository.ListTransactions(context.Background(), &listTransactionsParams)
 	assert.Nil(t, err)
 	assert.Equal(t, len(transactions), numberOfTransactions)
+}
+
+func TestTransactionCreateTransaction(t *testing.T) {
+	conn, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
+	require.Nil(t, err)
+	defer conn.Close()
+	prepareTestDatabase()
+
+	// transaction data
+	entityID := "b582a786-48ec-469b-b655-17cf779b9ce1"
+	entityType := "project"
+	externalSourceType := "bill.com"
+	transactionCategory := "donation"
+	exAccountID := "exaccountid1234"
+
+	// transaction line item data
+	lineItemAmountOne := int64(1500)
+	lineItemDescriptionOne := "donation"
+
+	lineItemAmountTwo := int64(-500)
+	lineItemDescriptionTwo := "fee"
+
+	lineItemOne := models.CreateLineItem{}
+	lineItemOne.Amount = &lineItemAmountOne
+	lineItemOne.Description = &lineItemDescriptionOne
+
+	lineItemTwo := models.CreateLineItem{}
+	lineItemTwo.Amount = &lineItemAmountTwo
+	lineItemTwo.Description = &lineItemDescriptionTwo
+
+	createTransaction := models.CreateTransaction{}
+	createTransaction.EntityID = &entityID
+	createTransaction.EntityType = &entityType
+	createTransaction.ExternalTransactionID = &exAccountID
+	createTransaction.Asset = "usd"
+	createTransaction.ExternalSourceType = &externalSourceType
+	createTransaction.TransactionCategory = transactionCategory
+	createTransaction.ExternalAccountID = &exAccountID
+	createTransaction.LineItems = []*models.CreateLineItem{}
+	createTransaction.LineItems = append(createTransaction.LineItems, &lineItemOne)
+	createTransaction.LineItems = append(createTransaction.LineItems, &lineItemTwo)
+
+	transactionRepository := NewRepository(conn)
+	transaction, err := transactionRepository.CreateTransaction(context.Background(), &createTransaction)
+	assert.Nil(t, err)
+	assert.Equal(t, transaction.Asset, createTransaction.Asset)
+	assert.Equal(t, transaction.ExternalTransactionID, exAccountID)
+	assert.Equal(t, len(transaction.LineItems), len(createTransaction.LineItems))
+
+	for _, lineItem := range transaction.LineItems {
+		if lineItem.Description == lineItemDescriptionOne {
+			assert.Equal(t, lineItem.Amount, lineItemAmountOne)
+		} else {
+			assert.Equal(t, lineItem.Amount, lineItemAmountTwo)
+		}
+	}
+
 }
