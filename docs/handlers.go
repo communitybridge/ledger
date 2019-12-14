b2ebd57aae3c154044d96bdf2e5d7b43e9234ae5
Paul Ryan <pol.oriain@gmail.com>
2019-12-14 16:05:54 +0900

    update README, change how we get the swagger JSON for redoc
    Signed-off-by: Paul Ryan <pol.oriain@gmail.com>

--- docs/handlers.go
+++ docs/handlers.go
@@ -12,4 +12,8 @@ func Configure(api *operations.LedgerAPI) {
 	api.DocsGetDocHandler = docs.GetDocHandlerFunc(func(params docs.GetDocParams) middleware.Responder {
 		return NewGetDocOK()
 	})
+
+	api.DocsGetSwaggerHandler = docs.GetSwaggerHandlerFunc(func(params docs.GetSwaggerParams) middleware.Responder {
+		return NewGetSwaggerOK()
+	})
 }
